92b89b4e2bfac17b5d03b9ba0247a1bd3c854d07
Aditya Kumar <aditya.95080@gmail.com>
2026-05-02 20:54:56 +0530

    added

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,6 @@ const app=express();
 app.get("/",()=>{
   console.log("hello from Docker");
 })
-app.listen(5060,()=>{
-  console.log("server connected at port of 5060");
+app.listen(5000,()=>{
+  console.log("server connected at port of 5000");
 });
